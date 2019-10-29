e86dbedb695bde5a984d8a08d11e23e4077325db
jalley64 <49925229+jalley64@users.noreply.github.com>
2019-10-29 10:01:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,12 @@
+const GameStrings = require("./GameStrings");
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
-var _signedUpUsers = [];
+var _signedUpUsers = ["<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>","<@250667798076850177>"];
+var _livingPlayers = [];
+var _playerRoles = {};
+var _deadPlayers = [];
 var _isGame = false;
 var _startDate = new Date();
 var _wolves = [];
@@ -12,8 +17,8 @@ var _isDay = true;
 var _signupsStarted = false;
 var _playercount = 13;
 //whenever adding to this list, add to end game reset
-var roles= {};
-
+var _votehistory = {};
+var _daycount;
 
 
 // still need to add these above to end game
@@ -24,7 +29,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     var message = msg.content;
-    var userID = msg.author;
+    var userID = "<@" + msg.author.id + ">";
     var longMessage = "";
     if (message.substring(0, 1) == '!') {
         if (message.startsWith("!startsignups")){
@@ -32,10 +37,14 @@ client.on('message', msg => {
             message = "!startsignups";
         }
 
-        if (message.startsWith("!cancelSignup") || message.startsWith("!singup")){ //add mod check and logic to prevent other users from removing people
-            var msgParts = longMessage.split(" ");
+        if (message.startsWith("!cancelsignup") || message.startsWith("!lynch") || message.startsWith("!signup")){ //add mod check and logic to prevent other users from removing people
+            var msgParts = message.split(" ");
             if (msgParts.length > 1){
                 userID = msgParts[1];
+                message = msgParts[0];
+            }
+            else if (message.startsWith("!lynch")){
+                msg.reply("You must specify a player to lynch.");
             }
         }
 
@@ -48,10 +57,13 @@ client.on('message', msg => {
                cancelSignUp(userID, msg);
                break;
             case '!rollcall': //modonly
-                msg.reply(_playercount);
+                printPlayers(msg);
                 break;  
             case '!startsignups':
                 tryStartSignups(msg, longMessage);
+                break;
+            case '!startgame':
+                startGame(msg);
                 break;
             //this one is for debuging
             case '!playercount':
@@ -60,10 +72,104 @@ client.on('message', msg => {
             case '!endgame':
                 endGame(msg);
                 break;
-            break;
+            case '!lynch':
+                applyVote(msg);
+                break;
          }
      }
   });
+
+function applyVote(msg){
+
+}
+
+function startGame(msg){
+    //todo: add check to make sure signups have been opened and that the player count has been met
+    if (_isGame){
+        msg.reply("We're already playing fuck face.");
+    }
+    else{
+        //print game is starting
+        _isGame = true;
+        _livingPlayers = _signedUpUsers;
+        
+        //generate roles
+        generateRoles();
+        //send out pms
+        pmRoles();
+        //print rules
+        var rules = GameStrings.GetDay1Rules(_signedUpUsers);
+        msg. reply(rules);
+        printPlayers(msg);
+    }
+}
+
+function startDay(msg){
+
+}
+
+function pmRoles(){
+    for (var key in _playerRoles){
+        var cleanKey = key.substr(2, key.length - 3);    
+        var body = ""
+        switch (_playerRoles[key]){
+            case "Seer":
+                body = GameStrings.GetSeerRoleMessage();
+                break;
+            case "Angel":
+                body = GameStrings.GetAngelRoleMessage();
+                break;
+            case "Vigilante":
+                body = GameStrings.GetVigilanteRoleMessage();
+                break;
+            case "Wolf":
+                body = GameStrings.GetWolfRoleMessage();
+                break;
+            case "Villager":
+                body = GameStrings.GetVillagerRoleMessage();
+                break;
+        }
+
+        client.users.get(cleanKey).send(body);
+    }
+}
+
+function generateRoles(){
+    var seerIndex = getRandomInt(0, _signedUpUsers.length - 1);
+    _seer = _signedUpUsers[seerIndex];
+    _playerRoles[_seer] = "Seer";
+    _signedUpUsers.splice(seerIndex,1);
+    
+    var angelIndex = getRandomInt(0, _signedUpUsers.length - 1);
+    _angel = _signedUpUsers[angelIndex];
+    _playerRoles[_angel] = "Angel";
+    _signedUpUsers.splice(angelIndex,1);
+    
+    var vigIndex = getRandomInt(0, _signedUpUsers.length - 1);
+    _vigilante = _signedUpUsers[vigIndex];
+    _playerRoles[_vigilante] = "Vigilante";
+    _signedUpUsers.splice(vigIndex,1);
+
+    for (var i = 0; i < 1; i++){
+        var wolfIndex = getRandomInt(0, _signedUpUsers.length - 1);
+        var wolf = _signedUpUsers[wolfIndex];
+        _wolves.push(wolf);
+        _playerRoles[wolf] = "Wolf";
+        _signedUpUsers.splice(wolfIndex,1);
+    }
+
+   /* _signedUpUsers.forEach(function(user){
+        _playerRoles[user] = "Villager";
+    }) */
+
+    _signedUpUsers = _livingPlayers;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function tryStartSignups(msg, longMessage){
     if (_signupsStarted || _isGame){
@@ -84,6 +190,10 @@ function tryStartSignups(msg, longMessage){
 }//modonly
 
 function cancelSignUp(userID, msg){
+    if (!_signupsStarted){
+        msg.reply("Signups aint open yet fuck face!");
+        return;
+    }
     if (!_signedUpUsers.includes(userID)){
         msg.reply("Ya aint even signed up ya filthy animal.")
     }
@@ -91,7 +201,7 @@ function cancelSignUp(userID, msg){
         var index = _signedUpUsers.indexOf(userID);
         if(index > -1){
           _signedUpUsers.splice(index, 1);  
-          msg.reply("Boo. Fine. We removed ya. Now get outta myface, see.");
+          msg.reply("Boo. Fine. We removed ya. Now get outta my face, see.");
         }
     }
 }
@@ -127,13 +237,13 @@ function signUpUser(userID, msg){
     }
 
     if (_signedUpUsers.includes(userID)){
-        msg.reply("Yo dipshit, you did this already!" + userID);
+        msg.reply("Yo dipshit, you did this already!");
     }
     else{
         if (_signedUpUsers.length < _playercount)
         {
             _signedUpUsers.push(userID);
-            msg.reply("Congrats! You are signed up to play werewolf. I will PM you when the game is set to begin");
+            msg.reply("Congrats! " + userID + " is signed up to play werewolf. I will send out PMs when the game is set to begin");
         }
         else{
             msg.reply("We are at capacity fella, navigate yourself outta my face.");
@@ -142,7 +252,26 @@ function signUpUser(userID, msg){
 }//modonly
 
 function printPlayers(msg){
+    var playerString = "";
+    if(_signupsStarted){
+        if (_isGame){
+            playerString = "**Current Living Players**\n"
+            _livingPlayers.forEach(function(element){
+                playerString += element + "\n";
+            });
+        }
+        else{
+            playerString = "**Current Signed Up Players**\n"
+            _signedUpUsers.forEach(function(element){
+                playerString += element + "\n";
+            });
+        }
+    }
+    else{
+        playerString = "Signups aint started yet boss man!";
+    }
 
+    msg.reply(playerString);
 }
 
   
